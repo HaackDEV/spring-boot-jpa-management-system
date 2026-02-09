24066879e0cd3bb6470c6c6885e0a626a7a8537e
Ruan Haack <ruan.haack03@gmail.com>
2026-02-09 20:45:44 -0300

    Service layer, component registration

---
+++ src/main/java/com/haackdev/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.haackdev.course.services;
+
+import com.haackdev.course.entities.User;
+import com.haackdev.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> FindAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
