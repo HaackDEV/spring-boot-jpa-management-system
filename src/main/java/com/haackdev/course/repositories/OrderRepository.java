69e42bb892a8b6a86299c02d307217c09a80d807
Ruan Haack <ruan.haack03@gmail.com>
2026-02-09 21:54:20 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/haackdev/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.haackdev.course.repositories;
+
+import com.haackdev.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/com/haackdev/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.haackdev.course.services;
+
+import com.haackdev.course.entities.Order;
+import com.haackdev.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> FindAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
